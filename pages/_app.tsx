350db73224c7ddf7a8b8c6d6e8173a445f222134
Jeff Park <jypark03@gmail.com>
2022-12-05 11:32:23 -0500

    analytics and light mode edits

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,29 @@
 import '../styles/globals.css'
+import Script from 'next/script'
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Script
+        strategy='afterInteractive'
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.ANALYTICS_ID}`}
+      />
+      <Script
+        id='google-analytics'
+        strategy='afterInteractive'
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.ANALYTICS_ID}', {
+            page_path: window.location.pathname,
+            });
+          `,
+        }}
+      />
+      <Component {...pageProps} />
+    </>
+  )
 }
